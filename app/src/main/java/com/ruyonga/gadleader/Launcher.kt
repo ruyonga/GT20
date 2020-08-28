8e83627363301200ee1aac60782be9026f18171f
Ruyonga Daniel <druyonga@gmail.com>
2020-08-28 21:55:52 +0300

    first commit

---
+++ app/src/main/java/com/ruyonga/gadleader/Launcher.kt
@@ -0,0 +1,11 @@
+package com.ruyonga.gadleader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Launcher : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
